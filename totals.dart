a86a100d571518e504143b3f44e1753dbd4243b9
David Castello <davidcastellobatetgmail.com>
2021-09-16 13:27:02 +0200

    logical implementation

--- totals.dart
+++ totals.dart
@@ -7,7 +7,22 @@ void main(List<String> arguments) {
   }
   final inputFile = arguments.first;
   final lines = File(inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double>{};
+  lines.removeAt(0);
   for (var line in lines) {
-    print(line);
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    //final duration = double.parse(values[3]);
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    previousTotal == null
+        ? totalDurationByTag[tag] = duration
+        : totalDurationByTag[tag] = previousTotal + duration;
+  }
+  for (var entry in totalDurationByTag.entries) {
+    final durationFormated = entry.value.toStringAsFixed(1);
+    final tag = entry.key;
+    print('$tag :  $durationFormated hours');
   }
 }
